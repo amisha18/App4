aa8c3b07adfce80faa636fe70c1119b4bab2f996
amisha18 <errupalsharma43@gmail.com>
2018-06-05 11:30:56 +0530

    added log

--- app/src/main/java/com/example/hp/myapp1/MainActivity.java
+++ app/src/main/java/com/example/hp/myapp1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.hp.myapp1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,6 +38,7 @@ private int counter=5;
     private void validate(String userName,String userPassword) {
         if ((userName.equals("Amisha")) && (userPassword.equals("234"))) {
             Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            Log.d("myTag", "success");
             startActivity(intent);
 
 
